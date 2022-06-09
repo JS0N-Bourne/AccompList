a5fef7bef16fa14d94d04b8408111dd724444538
Russell M <99734077+Java-Monsta@users.noreply.github.com>
2022-06-08 23:45:10 -0500

    Latest updates to AccompList app

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ for (i=0; i<accordion.length; i++) {
     this.classList.toggle('active')
   })
 }
+
+
+var divs = ["Dashboard", "Settings", "MyProfile", "MyDocuments", "MyCalendar", "MyFriends", "MySearches", "MyNotes", "Checklist", "Amazon", "Networking", "BehavioralQuestions", "TechnicalQuestions", "JavascriptGeneral", "Node", "CSTheory", "QuestionsforInterviewers", "Resources"];
+var visibleId = null;
+function show(id) {
+  if(visibleId !== id) {
+    visibleId = id;
+  } 
+  hide();
+}
+function hide() {
+  var div, i, id;
+  for(i = 0; i < divs.length; i++) {
+    id = divs[i];
+    div = document.getElementById(id);
+    if(visibleId === id) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
+} 
